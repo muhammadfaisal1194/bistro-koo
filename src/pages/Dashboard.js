0b053013167d2a54fcef9eaa914c2c33b37bc193
Beeni <beeni4429@gmail.com>
2022-12-30 18:09:24 +0500

    cart issue

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,25 @@
 import React from "react";
-import { useLocation, useNavigate, Outlet, Link  } from "react-router-dom";
+import { useLocation, useNavigate, Outlet, Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faShoppingBasket } from '@fortawesome/free-solid-svg-icons'
+import { faShoppingBasket, faBell } from '@fortawesome/free-solid-svg-icons'
+import { io } from "socket.io-client";
+import { API_URL } from "../utils/api";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { API_URL_SOCKET } from "../utils/api";
+import { useSelector } from "react-redux";
+import { Tooltip } from "@mui/material";
 
 const Dashboard = () => {
-  const { pathname } = useLocation();
   let navigate = useNavigate();
   const role = localStorage.getItem("role");
+  const { pathname } = useLocation();
+  const [notifications, setNotifications] = useState([]);
+  const isMobile = window.innerWidth <= 500;
+  const [count, setCount] = useState(0);
+  const cart = useSelector((state) => state.cart.cart);
+  const token = localStorage.getItem("accessToken");
+  let table = JSON.parse(localStorage.getItem("table"));
   const allProducts = () => {
     navigate("/dashboard/allproducts");
   };
@@ -28,18 +41,68 @@ const Dashboard = () => {
   const subcategories = () => {
     navigate("/dashboard/subcategories");
   };
+
+  const clickHandler = () => {
+    navigate("/cart");
+  };
+  const clickBellHandler = () => {
+    navigate("/dashboard/chat");
+  };
+  const logoutHandler = () => {
+    localStorage.removeItem("role");
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("user");
+  };
+  const audio = new Audio({
+    loop: true,
+    volume: 0.2,
+    src: ["/sound.mp3"],
+  });
+  const socket = io(API_URL_SOCKET);
+  socket.on("sendNotification", function (details) {
+    audio.play();
+    fetchAllNotification();
+  });
+  const fetchAllNotification = async () => {
+    const response = await axios.get(`${API_URL}/notification/index`);
+    setNotifications(response.data.data);
+  };
+
+  useEffect(() => {
+    fetchAllNotification();
+  }, []);
+
+  useEffect(() => {
+    let c = 0;
+    cart.map((car) => {
+      c += car.quantity;
+    });
+
+    setCount(c);
+  }, [cart]);
   return (
     <>
 
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid d-flex justify-content-between ">
-         <div></div>
+          <div>
+          </div>
           <div >
             <img src="/assets/logo.png" alt="" width="80" height="80" />
           </div>
 
-          <div className='border border-1'>
-            <FontAwesomeIcon icon={faShoppingBasket} style={{ "height": "34px" }} />
+          <div className="border border-1 p-1 position-relative">
+            <FontAwesomeIcon
+              icon={faBell}
+              style={{ cursor: "pointer", height: 34 }}
+              onClick={(e) => {
+                e.preventDefault();
+                clickBellHandler();
+              }}
+            />
+            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-dark">
+            {notifications.length}
+            </span>
           </div>
 
         </div>
